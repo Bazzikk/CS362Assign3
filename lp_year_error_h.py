6fd792872b8864c0dbf2ae0dd8f3e3f36fa008a7
Hector <hsandoval79@gmail.com>
2021-04-25 22:08:19 -0700

    created both leap year programs

---
+++ lp_year_error_h.py
@@ -0,0 +1,25 @@
+
+# To verify if a year is a leap year
+def leapYearVerifier (year):
+    if (year % 4 == 0):
+        if (year % 100 == 0):
+            if (year % 400 == 0):
+                print("It is a leap year")
+            else:
+                print("Not a leap year")
+
+        else:
+            print("It is a leap year")
+
+    else:
+        print("Not a leap year")
+
+
+
+
+# Enter year to verify
+val = input("Enter year: ")
+
+print("Verifying if year {} is a leap year".format(val))
+
+leapYearVerifier(int(val))
